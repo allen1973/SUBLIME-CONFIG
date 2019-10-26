c0942f72b4bd46ff2ddb795bee599e265aaa4331
Yujen Huang (日立人二) <yuhrenhuang@gmail.com>
2019-10-26 14:17:16 +0800

    3211

---
+++ Data/Backup/20161219104432/GotoBootstrap/GotoBootstrap.py
@@ -0,0 +1,17 @@
+import sublime, sublime_plugin, webbrowser
+
+class GotoBootstrapCommand(sublime_plugin.WindowCommand):
+	def run(self, page=''):
+		# open a new page
+		url = 'http://getbootstrap.com/'
+		if(page != ''):
+			url = url+page
+		webbrowser.open_new_tab(url)
+
+# class GotoBootsnippCommand(sublime_plugin.WindowCommand):
+# 	def run(self, page=''):
+# 		# open a new page
+# 		url = 'http://bootsnipp.com/'
+# 		if(page != ''):
+# 			url = url+page
+# 		webbrowser.open_new_tab(url)
